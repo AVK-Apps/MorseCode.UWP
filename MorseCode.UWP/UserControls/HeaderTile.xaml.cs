0a75be2b7f1cbb68eab4c40e394230182137c964
Vince <vince@adsc.com>
2023-08-17 15:31:38 -0400

    Change Learn Style and Header Tile

--- MorseCode.UWP/UserControls/HeaderTile.xaml.cs
+++ MorseCode.UWP/UserControls/HeaderTile.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using MorseCode.UWP.Classes;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -28,42 +30,23 @@ namespace MorseCode.UWP.UserControls
         public static readonly DependencyProperty DescriptionProperty =
             DependencyProperty.Register("Description", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
 
-        public object Source
-        {
-            get { return (object)GetValue(SourceProperty); }
-            set { SetValue(SourceProperty, value); }
-        }
-
-        public static readonly DependencyProperty SourceProperty =
-            DependencyProperty.Register("Source", typeof(object), typeof(HeaderTile), new PropertyMetadata(null));
-
-        public string Link
-        {
-            get { return (string)GetValue(LinkProperty); }
-            set { SetValue(LinkProperty, value); }
-        }
-
-        public static readonly DependencyProperty LinkProperty =
-            DependencyProperty.Register("Link", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
-
-
+        MediaPlayer mediaPlayer;
         public HeaderTile()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
-        private void Element_PointerEntered(object sender, PointerRoutedEventArgs e)
+        private void Button_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            //CreateOrUpdateSpringAnimation(1.1f);
-            (sender as UIElement).CenterPoint = new Vector3(70, 40, 1f);
-            (sender as UIElement).StartAnimation(_springAnimation);
-        }
-
-        private void Element_PointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            //CreateOrUpdateSpringAnimation(1.0f);
-            (sender as UIElement).CenterPoint = new Vector3(70, 40, 1f);
-            (sender as UIElement).StartAnimation(_springAnimation);
+            MemoryRandomAccessStream randomAccessStream = MorseHelper.GenerateMorsePlayBack((sender as Button).Tag.ToString(), App.Settings);
+            if (randomAccessStream != null)
+            {
+                mediaPlayer = new MediaPlayer
+                {
+                    Source = MediaSource.CreateFromStream(randomAccessStream, "wav")
+                };
+                mediaPlayer.Play();
+            }
         }
     }
 }
